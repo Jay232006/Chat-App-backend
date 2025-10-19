b65e1b2ba367492646944a8f2cac659b5aaae3b9
unknown <jayeshnc11@gmail.com>
2025-10-19 12:11:32 +0530

    added

--- src/controllers/chat.controllers.js
+++ src/controllers/chat.controllers.js
@@ -5,12 +5,28 @@ export const oldChats = async (req, res) => {
     const { userId } = req.body;
     if (!userId) {
         return res.status(400).json({ message: "User ID is required" });
-    };
+    }
 
-    let chat = await Chat.findOne({ users: { $all: [req.user._id, userId] },
-     }).populate("users","-password").populate("messages");
+    //checking if chat already exists
+    let chat = await Chat.findOne({
+        users: { $all: [req.user._id, userId] },
+    }).populate("users", "-password").populate("messages");
 
-     if(chat) return res.json(chat);
-
-     
+    if (chat) return res.json(chat);
+    
+    //New chat code
+    const newChat = await Chat.create({ users: [req.user._id, userId], messages: [] });
+    const fullChat = await Chat.findById(newChat._id).populate("users", "-password").populate("messages");
+    res.status(201).json(fullChat);
+    
 };
+//fetching chats
+export const fetchChats = async (req, res) => {
+    const chats = await Chat.find({ users: req.user.id })
+        .populate("users", "-password")
+        .populate("latestMessage")
+        .sort({ updatedAt: -1 });
+    res.json(chats);
+};
+
+
