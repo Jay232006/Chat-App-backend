8f9547c8fa5188c31671c0915d1cafa04a21d58e
unknown <jayeshnc11@gmail.com>
2025-10-19 13:54:15 +0530

    mionor

--- socket.test.js
+++ socket.test.js
@@ -4,7 +4,7 @@ try {
     const socket = await io("http://localhost:3000");
     socket.emit("setup", { _id: "68f48ddca83645fdbf9827d2" });
 
-    socket.on("connected", () => console.log("Socket connected ✅"));
+    socket.on("connected", () => console.log("Socket connected successfully"));
 } catch (error) {
   console.log("Socket connection error:", error);
 }
