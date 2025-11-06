c12b104bce3a9f52fa49fa07cab4fe59f3b0708f
Jay232006 <jayeshnc11@gmail.com>
2025-11-07 01:23:07 +0530

    ..

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,6 @@ import User from '../models/user.model.js';
 
 export async function getUsers(req, res) {
   try {
-    // return only _id and username (no sensitive fields)
     const users = await User.find({}, 'username').lean();
     res.json(users);
   } catch (err) {
