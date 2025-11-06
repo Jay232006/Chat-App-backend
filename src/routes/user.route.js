767c730589ef5a7c5a143d83115c33f604f59c81
Jay232006 <jayeshnc11@gmail.com>
2025-11-06 20:53:29 +0530

    ..

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,7 +4,6 @@ import { protect } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
-// GET /api/users
 router.get('/', getUsers);
 
 // GET /api/users/me - Get current authenticated user
