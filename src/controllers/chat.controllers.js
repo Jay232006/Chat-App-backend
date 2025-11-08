db6796360ea2d8dab5d92fba8323c2bb01a63d80
Jay232006 <jayeshnc11@gmail.com>
2025-11-08 12:32:41 +0530

    ..

--- src/controllers/chat.controllers.js
+++ src/controllers/chat.controllers.js
@@ -6,21 +6,18 @@ export const oldChats = async (req, res) => {
     if (!userId) {
         return res.status(400).json({ message: "User ID is required" });
     }
-
-    //checking if chat already exists
+    
     let chat = await Chat.findOne({
         users: { $all: [req.user._id, userId] },
     }).populate("users", "-password").populate("messages");
 
     if (chat) return res.json(chat);
     
-    //New chat code
     const newChat = await Chat.create({ users: [req.user._id, userId], messages: [] });
     const fullChat = await Chat.findById(newChat._id).populate("users", "-password").populate("messages");
     res.status(201).json(fullChat);
     
 };
-//fetching chats
 export const fetchChats = async (req, res) => {
     const chats = await Chat.find({ users: req.user.id })
         .populate("users", "-password")
