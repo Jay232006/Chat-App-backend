ecd26366fd310090788a7f171ecbb13131470d89
Jay232006 <jayeshnc11@gmail.com>
2026-01-05 00:15:56 +0530

    auth

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { register, login } from '../controllers/auth.controller.js';
 
 const router = express.Router();
+//diffferent auth method to be added 
 
 router.post('/register', register);
 
