8e4b4cf68a395e2ddc6255b8dd19a13d4df1b232
Jay232006 <jayeshnc11@gmail.com>
2026-01-05 00:15:12 +0530

    routes

--- src/routes/message.route.js
+++ src/routes/message.route.js
@@ -3,7 +3,7 @@ import { sendMessage, getMessages } from '../controllers/message.controller.js';
 import { protect } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
-
+//chat routes should be managed later 
 router.post('/', protect, sendMessage);
 
 router.get('/:chatId', protect, getMessages);
